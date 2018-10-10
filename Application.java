a1b843da73b1882746c8c1e1dd779036a04c2e5e
singhrahulnitn <31618697+singhrahulnitn@users.noreply.github.com>
2018-10-11 00:22:40 +0530

    comment syso line
    comment unnecessary syso line

--- Application.java
+++ Application.java
@@ -8,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 	
 	public static void main(String[] args) {
-		System.out.println("RAHUL");
+		//System.out.println("RAHUL");
 		SpringApplication.run(Application.class, args);
 	}
 }
