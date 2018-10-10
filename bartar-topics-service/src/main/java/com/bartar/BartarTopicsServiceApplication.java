085a4dff8372d7fd06ceb36c8722c9fed37eca42
Rahul Singh <singhrahulnitn@gmail.com>
2018-10-11 00:59:20 +0530

    Create Source Code

---
+++ bartar-topics-service/src/main/java/com/bartar/BartarTopicsServiceApplication.java
@@ -0,0 +1,12 @@
+package com.bartar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BartarTopicsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BartarTopicsServiceApplication.class, args);
+	}
+}
